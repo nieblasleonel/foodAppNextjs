975024460e24f8d698998659f490f0f534883782
Nieblas <nieblasleonel@gmail.com>
2024-04-18 09:57:55 -0500

    feat: home page

---
+++ components/main-header/mainHeader.jsx
@@ -0,0 +1,30 @@
+import Link from 'next/link'
+import React from 'react';
+import logoImg from '@/assets/logo.png';
+import style from './main-header.module.css';
+import Image from 'next/image';
+import { MainHeaderBackground } from './mainHeaderBackground';
+
+export const MainHeader = () => {
+  return (
+    <>
+      <header className={style.header}>
+        <Link href="/" className={style.logo}>
+          <Image
+            src={logoImg}
+            alt="A plate with food on it"
+            priority
+          />
+          Amazing food
+        </Link>
+        <nav className={style.nav}>
+          <ul>
+            <li><Link href="/meals">Browse Meals</Link></li>
+            <li><Link href="/community">Foodies Community</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <MainHeaderBackground />
+    </>
+  )
+}
